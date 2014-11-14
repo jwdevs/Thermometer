b987c578a13517f5cf52e7bc8283144d90d09206
Jakub Wozniak <jwozniak@Jakubs-MacBook-Pro.local>
2014-11-14 22:02:16 +0100

    Initial

--- Thermometer/Thermometer.ino
+++ Thermometer/Thermometer.ino
@@ -1,86 +1,74 @@
-/// 
-/// @mainpage	Thermometer 
-///
-/// @details	Thermometer sending data to the controller
-/// @n 		
-/// @n 
-/// @n @a		Developed with [embedXcode+](http://embedXcode.weebly.com)
-/// 
-/// @author		Jakub Wozniak
-/// @author		Jakub Wozniak
-/// @date		23/10/14 8:37 pm
-/// @version	<#version#>
-/// 
-/// @copyright	(c) Jakub Wozniak, 2014
-/// @copyright	All rights reserved
-///
-/// @see		ReadMe.txt for references
-///
+/*
+ Software serial multple serial test
+ 
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+ 
+ The circuit:
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+ 
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts,
+ so only the following can be used for RX:
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+ 
+ Not all pins on the Leonardo support change interrupts,
+ so only the following can be used for RX:
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+ 
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+ 
+ This example code is in the public domain.
+ 
+ */
+#include <SoftwareSerial.h>
+#include <Narcoleptic.h>
+SoftwareSerial xbee(6, 7); // RX, TX
+
+//Temperature sensor
+#include "TSIC.h"       // include the library
+
+#define TEMP_VCC 4
+#define TEMP_SIG 5
+#define INVALID_TEMP 255
+// instantiate the library, representing the sensor
+TSIC Sensor1(TEMP_SIG,TEMP_VCC);    // Signalpin, VCCpin
+//TSIC Sensor2(5, 2);  // Signalpin, VCCpin, NOTE: we can use the same VCCpin to power both sensors
+
+uint16_t temperature = 0;
+float tempInC = 0;
 
 
-///
-/// @file		Thermometer.ino
-/// @brief		Main sketch
-///
-/// @details	<#details#>
-/// @n @a		Developed with [embedXcode+](http://embedXcode.weebly.com)
-/// 
-/// @author		Jakub Wozniak
-/// @author		Jakub Wozniak
-/// @date		23/10/14 8:37 pm
-/// @version	<#version#>
-/// 
-/// @copyright	(c) Jakub Wozniak, 2014
-/// @copyright	All rights reserved
-///
-/// @see		ReadMe.txt for references
-/// @n
-///
-
-
-// Core library for code-sense - IDE-based
-#if defined(WIRING) // Wiring specific
-#include "Wiring.h"
-#elif defined(MAPLE_IDE) // Maple specific
-#include "WProgram.h"
-#elif defined(ROBOTIS)
-#include "libpandora_types.h"
-#include "pandora.h"
-#elif defined(MPIDE) // chipKIT specific
-#include "WProgram.h"
-#elif defined(DIGISPARK) // Digispark specific
-#include "Arduino.h"
-#elif defined(ENERGIA) // LaunchPad specific
-#include "Energia.h"
-#elif defined(MICRODUINO) // Microduino specific
-#include "Arduino.h"
-#elif defined(TEENSYDUINO) // Teensy specific
-#include "Arduino.h"
-#elif defined(RFDUINO) // RFduino specific
-#include "Arduino.h"
-#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
-#include "Arduino.h"
-#else // error
-#error Platform not defined
-#endif // end IDE
-
-// Include application, user and local libraries
-
-
-// Prototypes
-
-
-// Define variables and constants
-
-
-// Add setup code 
-void setup() 
-{
-
+float readTemp() {
+    float tempMean = 0;
+    if (Sensor1.getTemperture(&temperature)) {
+        return Sensor1.calc_Celsius(&temperature);
+    } else {
+        return INVALID_TEMP;
+    }
 }
 
-// Add loop code 
-void loop() 
+void setup()
 {
+    // Open serial communications and wait for port to open:
+    Serial.begin(9600);
+    // set the data rate for the SoftwareSerial port
+    xbee.begin(9600);
+}
 
+
+void loop() // run over and over
+{
+    tempInC = readTemp();
+    Serial.println("Temperature read:");
+    Serial.println(tempInC);
+    if(tempInC != INVALID_TEMP && tempInC < 40 && tempInC > -40) {
+        xbee.print(tempInC);
+    }
+    //Narcoleptic.delay(1000);
+    delay(1000);
 }
